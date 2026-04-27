7d1c2d8dbf73135350ec6c350813d5c635891401
Moonadsservice <infomoonads@gmail.com>
2026-04-26 19:02:49 -0700

    feat: move icons to public folder and ensure consistency across PWA and Favicon

--- arc-v1/vite.config.js
+++ arc-v1/vite.config.js
@@ -17,17 +17,17 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/assets/images/icon.png',
+            src: 'assets/images/icon.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/assets/images/icon.png',
+            src: 'assets/images/icon.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/assets/images/icon.png',
+            src: 'assets/images/icon.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
